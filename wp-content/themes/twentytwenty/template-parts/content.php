ea6891d2226ed30f49a22d1218accf50c3069274
andrea <andreacazzola90@gmail.com>
2020-12-14 20:39:49 +0100

    include post custom template

--- wp-content/themes/twentytwenty/template-parts/content.php
+++ wp-content/themes/twentytwenty/template-parts/content.php
@@ -25,6 +25,8 @@
 
 	?>
 
+ 	<?php include get_template_directory() . '../../customTemplates/post.php'; ?>
+
 	<div class="post-inner <?php echo is_page_template( 'templates/template-full-width.php' ) ? '' : 'thin'; ?> ">
 
 		<div class="entry-content">
